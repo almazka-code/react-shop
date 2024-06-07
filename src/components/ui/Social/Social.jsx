bc68d04867c2a94b1defcf07a64b461f71ccff8a
Olga Smirnova <almazkacode@gmail.com>
2024-06-07 14:53:02 +0500

    refactor: change the way icons are displayed in Social component

--- src/components/ui/Social/Social.jsx
+++ src/components/ui/Social/Social.jsx
@@ -1,12 +1,28 @@
 import styles from './Social.module.scss';
 
+import instagramIcon from '../../../assets/svg/social/instagram.svg';
+import facebookIcon from '../../../assets/svg/social/facebook.svg';
+import twitterIcon from '../../../assets/svg/social/twitter.svg';
+import telegramIcon from '../../../assets/svg/social/telegram.svg';
+
 const Social = () => {
   const socialLinks = [
-    { name: 'instagram', url: 'https://www.instagram.com' },
-    { name: 'facebook', url: 'https://www.facebook.com' },
-    { name: 'twitter', url: 'https://www.x.com' },
-    { name: 'telegram', url: 'https://web.telegram.org' },
-    { name: 'vk', url: 'https://vk.com' },
+    {
+      name: 'instagram',
+      url: 'https://www.instagram.com',
+      icon: instagramIcon,
+    },
+    {
+      name: 'facebook',
+      url: 'https://www.facebook.com',
+      icon: facebookIcon,
+    },
+    { name: 'twitter', url: 'https://www.x.com', icon: twitterIcon },
+    {
+      name: 'telegram',
+      url: 'https://web.telegram.org',
+      icon: telegramIcon,
+    },
   ];
 
   return (
@@ -14,11 +30,12 @@ const Social = () => {
       {socialLinks.map((social) => (
         <li className={styles.social__item} key={social.name}>
           <a
-            className={`${styles.social__link} ${styles[social.name]}`}
+            className={styles.social__link}
             href={social.url}
             target="_blank"
             rel="noopener noreferrer"
-            aria-label={social.name}></a>
+            aria-label={social.name}
+            style={{ backgroundImage: `url(${social.icon})` }}></a>
         </li>
       ))}
     </ul>
