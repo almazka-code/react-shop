1a4785f967d08d15703dc3f87b6bab951351d1f2
Olga Smirnova <almazkacode@gmail.com>
2024-06-26 15:29:19 +0500

    fix: change brand select component

--- src/components/ui/Forms/FilterForm/BrandSelect/BrandSelect.jsx
+++ src/components/ui/Forms/FilterForm/BrandSelect/BrandSelect.jsx
@@ -1,17 +1,52 @@
 import styles from './BrandSelect.module.scss';
+import { useState } from 'react';
+
+// export const BrandSelect = () => {
+//   const brands = ['Все бренды', 'Apple', 'Huawei', 'Samsung', 'Xiaomi'];
+
+//   return (
+//     <label className={styles.label}>
+//       <select className={styles.select} type="text" name="category">
+//         {brands.map((brand) => (
+//           <option key={brand} value={brand}>
+//             {brand}
+//           </option>
+//         ))}
+//       </select>
+//     </label>
+//   );
+// };
 
 export const BrandSelect = () => {
   const brands = ['Все бренды', 'Apple', 'Huawei', 'Samsung', 'Xiaomi'];
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState(0);
+
+  const onClickListItem = (index) => {
+    setSelectedBrand(index);
+    setIsOpen(false);
+  };
+
   return (
-    <label className={styles.label}>
-      <select className={styles.select} type="text" name="category">
-        {brands.map((brand) => (
-          <option key={brand} value={brand}>
-            {brand}
-          </option>
-        ))}
-      </select>
-    </label>
+    <div className={styles.select} onClick={() => setIsOpen(!isOpen)}>
+      <div className={styles.wrapper}>
+        <span>{brands[selectedBrand]}</span>
+        <span className={styles.arrow}></span>
+      </div>
+
+      {isOpen && (
+        <ul className={styles.popup}>
+          {brands.map((value, index) => (
+            <li
+              key={value}
+              onClick={() => onClickListItem(index)}
+              className={`${styles.item} ${selectedBrand === index ? styles.active : ''}`}>
+              {value}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
